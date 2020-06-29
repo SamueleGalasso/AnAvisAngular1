bb0d45c120eae0bc09f7aede622fed48d690b9d1
Samuele Galasso <56586742+SamueleGalasso@users.noreply.github.com>
2020-06-29 12:47:38 +0200

    Update DateRepository.java

--- src/main/java/com/anavis/repository/DateRepository.java
+++ src/main/java/com/anavis/repository/DateRepository.java
@@ -7,6 +7,6 @@ import org.springframework.data.repository.CrudRepository;
  * Interfaccia che ci permette di utilizzare alcuni metodi fondamentali per comunicare col db.
  */
 public interface DateRepository extends CrudRepository<Date, Long>{
-       //List<Prenotation> findByTitleContaining(String keyword);
+       
 
 }
