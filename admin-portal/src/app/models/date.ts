d3ad872e59ec592e3121039f15ec468938d89633
Samuele Galasso <56586742+SamueleGalasso@users.noreply.github.com>
2020-06-29 23:59:56 +0200

    Delete date.ts

--- admin-portal/src/app/models/date.ts
+++
@@ -1,9 +0,0 @@
-export class Date {
-
-public id: number;
-public place: string;
-public prenotationDate: string;
-public active: boolean;
-public description: string;
-public remainingNumber: number;
-}
